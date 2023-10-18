d30b69d0a61b9c18c7eea27c5ff1e6a300c075c1
JuanMGallego <jmgallego@iesnervion.es>
2023-10-18 14:42:48 +0200

    Empezado Unidad 6 Ejercicio 1 y 2

---
+++ Unidad06_Ejercicios/Unidad06_Ejercicio01/Controllers/HomeController.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Unidad06_Ejercicio01.Controllers
+{
+    public class HomeController : Controller
+    {
+        public String Index()
+        {
+            return "Juanma";
+        }
+
+        public String Apellidos()
+        {
+            return "Gallego Giron";
+        }
+
+        public ActionResult Saludo()
+        {
+            return View();
+        }
+    }
+}
---
+++ Unidad06_Ejercicios/Unidad06_Ejercicio01/Controllers/ProductosController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Unidad06_Ejercicio01.Controllers
+{
+    public class ProductosController : Controller
+    {
+        public IActionResult ListadoProductos()
+        {
+            return View();
+        }
+    }
+}

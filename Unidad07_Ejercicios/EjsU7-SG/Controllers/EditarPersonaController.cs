259ca43cf557fc7c66aa3bc26f8bddf88a9720e5
JuanMGallego <jmgallego@iesnervion.es>
2023-11-15 14:40:12 +0100

    sada

---
+++ Unidad07_Ejercicios/EjsU7-SG/Controllers/EditarPersonaController.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using EjsU7_SG.Models.ViewModel;
+
+namespace EjsU7_SG.Controllers
+{
+    public class EditarPersonaController : Controller
+    {
+        /// <summary>
+        /// Action result para la vista EditarPersona
+        /// A la vista se le envía una ViewModel clsPersonaDepartamento, que contiene una lista de personas con departamentos
+        /// y otra de departamentos
+        /// Post: Siempre devuelve algo
+        /// </summary>
+        /// <returns>Devuelve la vista EditarPersona</returns>
+        public IActionResult EditarPersona()
+        {
+            return View(new clsPersonaDepartamento());
+        }
+    }
+}
---
+++ Unidad07_Ejercicios/EjsU7-SG/Controllers/HomeController.cs
@@ -0,0 +1,61 @@
+﻿using EjsU7_SG.Models.Entidades;
+using EjsU7_SG.Models.DAL;
+using Microsoft.AspNetCore.Mvc;
+using System.Diagnostics;
+
+namespace EjsU7_SG.Controllers
+{
+    public class HomeController : Controller
+    {
+        /// <summary>
+        /// ActionResult para la vista Index. Creamos una variable que recoge la hora actual del día y
+        /// programamos un sistema para enviar un saludo en función de la hora del día.
+        /// La vista recibe el mensaje mediante un ViewData, recibe la fecha en versión larga mediante un ViewBag 
+        /// y un objeto persona mediante el modelo
+        /// </summary>
+        /// <returns>Devuelve la vista Index</returns>
+        public ActionResult Index()
+        {
+
+            var fechaActual = DateTime.Now;
+            var message = "Buenos días";
+
+            if (fechaActual.Hour >= 14)
+            {
+                message = "Buenas tardes";
+            } else if (fechaActual.Hour >= 21)
+            {
+                message = "Buenas noches";
+            }
+
+            ViewData["Message"] = message;
+            ViewBag.fechaLarga = fechaActual.ToLongDateString();
+            return View(persona);
+        }
+
+        /// <summary>
+        /// ActionResult en el que se ha generado una lista de objetos clsPersona
+        /// La vista recibe la lista mediante una ViewBag
+        /// Post: La lista no puede estar vacía 
+        /// </summary>
+        /// <returns>Devuelve la vista listadoPersonas</returns>
+        public IActionResult listadoPersonas()
+        {
+            return View(clsListadoPersonas.listadoPersonas());
+        }
+
+        /// <summary>
+        /// Creamos un objeto persona porque podemos 
+        /// </summary>
+        private clsPersona persona = new clsPersona()
+        {
+            Id = 1,
+            Nombre = "Francisco",
+            Apellidos = "Rodríguez",
+            Direccion = "Mi calle",
+            FechaNac = new DateTime(2003, 04, 27),
+            Tlfn = 654444089,
+        };
+    }
+
+}

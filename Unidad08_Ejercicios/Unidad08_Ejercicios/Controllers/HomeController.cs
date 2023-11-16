e45c27c1b737adebfb6d1c69cf81438709e35137
JuanMGallego <jmgallego@iesnervion.es>
2023-11-16 14:40:12 +0100

    empezado u8

---
+++ Unidad08_Ejercicios/Unidad08_Ejercicios/Controllers/HomeController.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Diagnostics;
+using Unidad08_Ejercicios.Models;
+using Unidad08_Ejercicios.Models.Entities;
+
+namespace Unidad08_Ejercicios.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Editar()
+        {
+            // Supongamos que tienes una instancia de clsPersona con tus datos
+            clsPersona persona = new clsPersona
+            {
+                IdPersona = 1,
+                Nombre = "TuNombre",
+                Apellidos = "TusApellidos"
+                // Agrega otros datos según sea necesario
+            };
+
+            return View(persona);
+        }
+
+        // Acción para procesar el formulario y mostrar la vista PersonaModificada
+        [HttpPost]
+        public ActionResult PersonaModificada(clsPersona personaModificada)
+        {
+            // Aquí puedes realizar las operaciones necesarias para guardar los cambios en la base de datos, por ejemplo.
+            // En este ejemplo, simplemente mostraremos la vista PersonaModificada con los datos modificados.
+            return View(personaModificada);
+        }
+    }
+}
---
+++ Unidad08_Ejercicios/Unidad08_Ejercicios/Models/Entities/clsPersona.cs
@@ -0,0 +1,12 @@
+﻿namespace Unidad08_Ejercicios.Models.Entities
+{
+    public class clsPersona
+    {
+
+        public int IdPersona {  get; set; }
+        public String Nombre {  get; set; }
+        public String Apellidos { get; set; }
+
+        
+    }
+}

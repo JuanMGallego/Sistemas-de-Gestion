3893c38293f4c0351674d42183701beba0a3a877
JuanMGallego <jmgallego@iesnervion.es>
2023-11-20 14:15:52 +0100

    Mandaloriano empezado

---
+++ Mandaloriano/Mandaloriano/Controllers/MisionController.cs
@@ -0,0 +1,33 @@
+﻿using Mandaloriano.DAL;
+using Mandaloriano.Models.Entities;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+
+namespace Mandaloriano.Controllers
+{
+    public class MisionController : Controller
+    {
+        private MisionDAL misionDAL;
+
+        public MisionController()
+        {
+            misionDAL = new MisionDAL();
+        }
+
+        public ActionResult Index()
+        {
+            List<MisionDAL> misiones = misionDAL.ObtenerMisiones();
+            ViewBag.Misiones = new SelectList(misiones, "Id", "Nombre");
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult VerDetalles(int id)
+        {
+            Mision misión = misionDAL.ObtenerDetallesMision(id);
+            ViewBag.DetallesMision = misión;
+            return View("Detalles");
+        }
+    }
+}
---
+++ Mandaloriano/Mandaloriano/DAL/MisionDAL.cs
@@ -0,0 +1,25 @@
+﻿using Mandaloriano.Models.Entities;
+
+namespace Mandaloriano.DAL
+{
+    public class MisionDAL
+    {
+        private static List<Mision> misiones = new List<Mision>
+        {
+        new Mision { Id = 1, Nombre = "Rescate de Baby Yoda", Descripcion = "Debes hacerte con Grogu y llevárselo a Luke SkyWalker para su entrenamiento.", Recompensa = "5000 créditos" },
+        new Mision { Id = 2, Nombre = "Recuperar armadura Beskar", Descripcion = "Tu armadura de Beskar ha sido robada. Debes encontrarla.", Recompensa = "2000 créditos" },
+        new Mision { Id = 3, Nombre = "Planeta Sorgon", Descripcion = "Debes llevar a un niño de vuelta a su planeta natal “Sorgon”.", Recompensa = "500 créditos" },
+        new Mision { Id = 4, Nombre = "Renacuajos", Descripcion = "Debes llevar a una Dama Rana y sus huevos de Tatooine a la luna del estuario Trask, donde su esposo fertilizará los huevos.", Recompensa = "500 créditos" }
+        };
+
+        public List<Mision> ObtenerMisiones()
+        {
+            return misiones;
+        }
+
+        public Mision ObtenerDetallesMision(int id)
+        {
+            return misiones.Find(m => m.Id == id);
+        }
+    }
+}

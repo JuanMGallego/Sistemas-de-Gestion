b7e7561a3876d4baac8801e4c7bc127861729226
JuanMGallego <jmgallego@iesnervion.es>
2023-11-23 14:40:13 +0100

    Unidad 10 ejercicios empezados

---
+++ Unidad10_Ejercicios/Unidad10_Ejercicios/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Unidad10_Ejercicios.Models
+{
+	public class ErrorViewModel
+	{
+		public string? RequestId { get; set; }
+
+		public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+	}
+}

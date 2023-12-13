9256dfc537e4d0b8b622399eb7915e4e8094f02b
JuanMGallego <jmgallego@iesnervion.es>
2023-12-13 14:39:11 +0100

    CRUD con API

---
+++ CRUD/CRUD/Controllers/API/PersonasController.cs
@@ -0,0 +1,47 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using BL;
+using Entidades;
+using DAL.Listados;
+using BL.Listados;
+
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace CRUD.Controllers.API
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class PersonasController : ControllerBase
+    {
+        // GET: api/<PersonasController>
+        [HttpGet]
+        public IEnumerable<clsPersona> Get()
+        {
+            return ListaPersonasBL.listadoCompletoPersonasBL();
+        }
+
+        // GET api/<PersonasController>/5
+        [HttpGet("{id}")]
+        public string Get(int id)
+        {
+            return "value";
+        }
+
+        // POST api/<PersonasController>
+        [HttpPost]
+        public void Post([FromBody] string value)
+        {
+        }
+
+        // PUT api/<PersonasController>/5
+        [HttpPut("{id}")]
+        public void Put(int id, [FromBody] string value)
+        {
+        }
+
+        // DELETE api/<PersonasController>/5
+        [HttpDelete("{id}")]
+        public void Delete(int id)
+        {
+        }
+    }
+}

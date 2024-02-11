27a25191946dca9ef9e71ebe8a094b20db68e37d
JuanMGallego <jmgallego@iesnervion.es>
2024-02-11 12:42:31 +0100

    loul

--- Mandaloriano/Mandaloriano3/obj/Debug/net7.0/Mandaloriano3.AssemblyInfo.cs
+++ Mandaloriano/Mandaloriano3/obj/Debug/net7.0/Mandaloriano3.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Mandaloriano3")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+9281bf4d962f6eee1113d51cd1c8cfd6f30eb206")]
 [assembly: System.Reflection.AssemblyProductAttribute("Mandaloriano3")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Mandaloriano3")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]

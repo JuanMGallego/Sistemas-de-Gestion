88756e1798718eb9240c545fd8164febca8e7ebd
JuanMGallego <jmgallego@iesnervion.es>
2024-02-20 00:51:44 +0100

    xdzcfds

--- AJAX/CRUD/javaScript.js
+++ AJAX/CRUD/javaScript.js
@@ -3,7 +3,6 @@ function pedirDatos()
 {
 
     var miLlamada = new XMLHttpRequest();
-
     miLlamada.open("GET", "https://crudnervion.azurewebsites.net/api/personas");
 
     //Definicion estados
@@ -11,15 +10,10 @@ function pedirDatos()
     miLlamada.onreadystatechange = function () {
 
         if (miLlamada.readyState < 4) {
-
         //aquí se puede poner una imagen de un reloj o un texto “Cargando”
-
         } else if (miLlamada.readyState == 4 && miLlamada.status == 200) {
-
             var arrayPersonas = JSON.parse(miLlamada.responseText);
-
             funcionQueHagaAlgoConLasPersonas(arrayPersonas);
-
         }
 
     };
